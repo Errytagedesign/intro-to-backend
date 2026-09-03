90e0fdc289ae6998818927ddabb4a1649c0d0a31
Codeeq <errytage@gmail.com>
2026-09-03 16:51:00 +0100

    add auth routes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new Schema(
   {
@@ -45,6 +46,27 @@ const userSchema = new Schema(
     timestamps: true,
   },
 );
+
+userSchema.pre('save', async function (params) {
+  // only hash if password is modified or new
+  if (!this.isModified('password')) return next();
+
+  try {
+    // generate salt and has password
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// method to compare passwords
+
+userSchema.methods.comparePassword = async function (userPassword) {
+  return await bcrypt.compare(userPassword, this.password);
+};
 
 userSchema.set('toJSON', {
   transform: (doc, ret) => {
